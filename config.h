3f5532e97b2f3c64286095062235e3c66cbf6657
Michał Matysek <hello@michalmatysek.com>
2023-08-30 20:19:24 +0200

    added SymbolsNerdFont

--- config.h
+++ config.h
@@ -10,7 +10,10 @@ static const unsigned int gappov    = 16;       /* vert outer gap between window
 static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Hack:size=10:antialias=true:autohint=true" };
+static const char *fonts[]          = {
+    "Hack:size=10:antialias=true:autohint=true",
+    "SymbolsNerdFont:size=11:antialias=true:autohint=true",
+};
 static const char dmenufont[]       = "Hack:size=10:antialias=true:autohint=true";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
